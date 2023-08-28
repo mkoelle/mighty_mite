b9508b5f592e50cc449bd0bbb173ed26562b1738
Mark Koelle <mkoelle@gmail.com>
2023-08-28 12:01:50 -0500

    change wifi strength -> color mapping

--- management-portal/src/lib/Network.svelte
+++ management-portal/src/lib/Network.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
   import { Indicator, Badge } from "flowbite-svelte";
-  export let ssid     = "Network SSID"
-  export let security = "Security"
-  export let channel = "Channel"
-  export let signal_strength = -50
+  export let ssid = "Network SSID";
+  export let security = "Security";
+  export let channel = "Channel";
+  export let signal_strength = -50;
   // export let Known    = false
-  const strengthToColor = (s:number) => {
+  const strengthToColor = (s: number) => {
     // -30 dBm is Maximum signal strength
-    if(s > -50){
-      // up to -50 is excelent
-      return "green"
-    } else if (s > -60){
+    if (s > -50) {
+      // up to -50 is excellent
+      return "green";
+    } else if (s > -60) {
       // -50 to -60 is good
-      return "indigo"
-    } else if (s > -67){
+      return "blue";
+    } else if (s > -67) {
       // -60 to -67  is minimum for streaming
-      return "yellow"
-    } else if (s > -70){
+      return "indigo";
+    } else if (s > -70) {
       // -67 to -70 is decent for simple things
-      return "purple"
-    } else if (s > -80){
+      return "purple";
+    } else if (s > -80) {
       // -70 to -80 is minimum to make a connection
-      return "blue"
+      return "yellow";
     }
-    return "red"
-  }
+    return "red";
+  };
 </script>
 
 <div class="flex items-center space-x-3">
@@ -32,9 +32,15 @@
     <p class="text-sm font-semibold text-gray-900 truncate dark:text-white">
       {ssid}
     </p>
-    <p class="text-sm text-gray-500 truncate dark:text-gray-400">Ch: {channel} Sec: {security}</p>
+    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+      Ch: {channel} Sec: {security}
+    </p>
   </div>
-  <Badge color="{strengthToColor(signal_strength)}" rounded class="px-2.5 py-0.5">
-    <Indicator color="{strengthToColor(signal_strength)}" size="xs" class="mr-1" />{signal_strength}
+  <Badge color={strengthToColor(signal_strength)} rounded class="px-2.5 py-0.5">
+    <Indicator
+      color={strengthToColor(signal_strength)}
+      size="xs"
+      class="mr-1"
+    />{signal_strength}
   </Badge>
 </div>
