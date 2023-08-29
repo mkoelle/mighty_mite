5051f59f2674aae81d65a1d0866d4aa7dda8c2dc
Mark Koelle <mkoelle@gmail.com>
2023-08-28 21:58:48 -0500

    Add storybook (#1)
    * add storybook
    * update packages
    * add props for network view
    * list networks
    * handle folders better in purge
    * change dist to www
    * update poetry.lock

---
+++ management-portal/src/lib/Network.stories.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import Network from './Network.svelte';
+  let defaults = {
+    ssid:"My Home Network",
+    security: "WEP",
+    channel: 11,
+    signal_strength : -50
+  }
+</script>
+
+<Meta title="Network" component={Network}/>
+
+<Template let:args>
+  <Network {...args}/>
+</Template>
+
+<Story name="Primary" args={defaults}/>
+
+<Story name="Low Signal" args={{...defaults, signal_strength: -90}}/>
---
+++ management-portal/src/lib/Network.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { Indicator, Badge } from "flowbite-svelte";
+  export let ssid = "Network SSID";
+  export let security = "Security";
+  export let channel = "Channel";
+  export let signal_strength = -50;
+  // export let Known    = false
+  const strengthToColor = (s: number) => {
+    // -30 dBm is Maximum signal strength
+    if (s > -50) {
+      // up to -50 is excellent
+      return "green";
+    } else if (s > -60) {
+      // -50 to -60 is good
+      return "blue";
+    } else if (s > -67) {
+      // -60 to -67  is minimum for streaming
+      return "indigo";
+    } else if (s > -70) {
+      // -67 to -70 is decent for simple things
+      return "purple";
+    } else if (s > -80) {
+      // -70 to -80 is minimum to make a connection
+      return "yellow";
+    }
+    return "red";
+  };
+</script>
+
+<div class="flex items-center space-x-3">
+  <div class="flex-1 min-w-0">
+    <p class="text-sm font-semibold text-gray-900 truncate dark:text-white">
+      {ssid}
+    </p>
+    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+      Ch: {channel} Sec: {security}
+    </p>
+  </div>
+  <Badge color={strengthToColor(signal_strength)} rounded class="px-2.5 py-0.5">
+    <Indicator
+      color={strengthToColor(signal_strength)}
+      size="xs"
+      class="mr-1"
+    />{signal_strength}
+  </Badge>
+</div>
